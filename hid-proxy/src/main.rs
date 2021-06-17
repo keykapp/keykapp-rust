811570f83a3e69e6d97d1c7f4b9f546681c7d196
Helder S Ribeiro <git@agentofuser.com>
2021-06-16 21:09:04 -0400

    use commitlog for event log

--- hid-proxy/src/main.rs
+++ hid-proxy/src/main.rs
@@ -1,8 +1,13 @@
+use commitlog::*;
 use rdev::{grab, Event, EventType};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let grab_keyboard = |event: Event| {
+        // open a directory called 'log' for segment and index storage
+        let opts = LogOptions::new("log");
+        let mut log = CommitLog::new(opts).unwrap();
+
         match event.event_type {
             // EventType::KeyPress(Key::Tab) => {
             //     println!("Blocked a Tab!");
@@ -14,7 +19,11 @@ fn main() -> Result<(), Box<dyn Error>> {
             // }
             // EventType::KeyRelease(Key::Tab) => None,
             EventType::KeyPress(_) | EventType::KeyRelease(_) => {
-                println!("{:?}", event);
+                log.append_msg(
+                    serde_json::to_string(&event)
+                        .expect("Could not serialize event."),
+                )
+                .expect("Could not log serialized event.");
                 Some(event)
             }
             _ => Some(event),
