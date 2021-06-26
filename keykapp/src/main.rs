d65cdc8d46fdfc7bb84c31cd3f7e579d2b4cddab
Helder S Ribeiro <git@agentofuser.com>
2021-06-26 04:46:25 -0400

    keep a global in-memory event log

--- keykapp/src/main.rs
+++ keykapp/src/main.rs
@@ -1,8 +1,33 @@
+use commitlog::message::*;
 use commitlog::*;
 use rdev::{grab, Event, EventType};
 use std::error::Error;
 
+fn read_event_log() -> Vec<Event> {
+    // open a directory called 'log' for segment and index storage
+    let opts = LogOptions::new("log");
+    let log = CommitLog::new(opts).unwrap();
+
+    // read the messages
+    let messages = log.read(0, ReadLimit::max_bytes(usize::MAX)).unwrap();
+    let events: Vec<Event> = messages
+        .iter()
+        .map(|message| {
+            serde_cbor::from_reader::<Event, &[u8]>(message.payload())
+                .expect("Could not deserialize event from log message.")
+        })
+        .collect();
+    events
+}
+
+static mut EVENT_LOG: Vec<Event> = Vec::new();
+
 fn main() -> Result<(), Box<dyn Error>> {
+    unsafe {
+        EVENT_LOG = read_event_log();
+        println!("EVENT_LOG.len() -> {}", EVENT_LOG.len());
+    }
+
     let grab_keyboard = |event: Event| {
         // open a directory called 'log' for segment and index storage
         let opts = LogOptions::new("log");
@@ -15,6 +40,10 @@ fn main() -> Result<(), Box<dyn Error>> {
                         .expect("Could not serialize event."),
                 )
                 .expect("Could not log serialized event.");
+                unsafe {
+                    EVENT_LOG.push(event.clone());
+                    println!("EVENT_LOG.len() -> {}", EVENT_LOG.len());
+                }
                 Some(event)
             }
             _ => Some(event),
